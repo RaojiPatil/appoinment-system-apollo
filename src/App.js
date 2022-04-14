f5aa4421a38930cea3d4664e47a123e93cfdca8c
Raoji Patil <raojipatil22@gmail.com>
2022-04-14 09:05:31 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { Landingpage } from './Components/landingpage';
+import { Routers } from './Routes/Routers';
 
 function App() {
   return (
     <div className="App">
+      {/* <Routers/> */}
       <Landingpage />
     </div>
   );
--- src/Components/landingpage.jsx
+++ src/Components/landingpage.jsx
@@ -23,13 +23,15 @@ export const Landingpage = () => {
             <div className='doctor1'>
 
 <img  src="https://images.unsplash.com/photo-1612349317150-e413f6a5b16d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGRvY3RvcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" />
-
+<h4>Dr: Om Patil</h4>
             </div>
             <div className='doctor2'>
 <img src="https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZG9jdG9yc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+<h4>Dr: larry jemms</h4>
             </div>
             <div className='doctor3'>
 <img src="https://images.unsplash.com/photo-1537368910025-700350fe46c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8ZG9jdG9yc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+<h4>Dr: petter pit</h4>
             </div>
         </div>
     </div>
